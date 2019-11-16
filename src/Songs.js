f51af7dad6167f038f57a40898fdeb625a4bc527
James <jameselson19@gmail.com>
2019-11-16 19:56:03 +0000

    fixed songs

--- src/Songs.js
+++ src/Songs.js
@@ -1,8 +1,15 @@
 import ohio from "./ohio.mp3";
+import s from "./s1.mp3";
+import m from "./m1.mp3";
+import o from "./o1.mp3";
+import b from "./b1.mp3";
 
 const songs = {
     "Ohio is for Emo Kids": ohio,
-
+    "Spooky Scary Skeletons": s,
+    "Baby Shark":b,
+    "Wii Theme Song":m,
+    "Old Town Road":o
 }
 
 export default songs;
