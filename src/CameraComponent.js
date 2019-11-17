862af683372d197be1f243e10d8dafa99b2150c6
James <jameselson19@gmail.com>
2019-11-17 04:59:00 +0000

    Merge branch 'master' of C:\Users\james_\WebstormProjects\oxfordhacks with conflicts.

--- src/CameraComponent.js
+++ src/CameraComponent.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Webcam from "react-webcam";
-import { Button, Container, Row, Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import ServerDetails from "./ServerDetails"
-import Songs from "./Songs";
-import Cookies from "universal-cookie";
 
 const videoConstraints = {
     width: 1280,
@@ -16,21 +14,18 @@ const CameraComponent = () => {
 
     const webcamRef = React.useRef(null);
 
-    var audio = new Audio(Songs["Wii Theme Song"]);
-    audio.loop = true;
-    
     const capture = React.useCallback(
         () => {
-            audio.play();
+
             var imgArr = [];
             var timesRun = 0;
-            var interval = setInterval(function () {
-                if (timesRun === 100) {
+            var interval = setInterval(function(){
+                if (timesRun === 210) {
                     clearInterval(interval);
                     var data = {
                         "images": imgArr
                     }
-                    fetch("http://" + ServerDetails.TOGETHER + "/getResponse", {
+                    fetch("http://"+ServerDetails.TOGETHER+"/getResponse", {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
@@ -38,7 +33,6 @@ const CameraComponent = () => {
                         },
                         body: JSON.stringify(data)
                     }).then((resp) => {
-                        audio.pause()
                         return resp;
                     }).then((json) => {
                         console.log(json);
@@ -53,21 +47,19 @@ const CameraComponent = () => {
         [webcamRef]
     );
 
-    return (
-        <Container>
-            <Col><Row><Webcam
-                audio={false}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                width={window.width}
-                videoConstraints={videoConstraints}
-                capture={capture}
-            />
-                <Button variant="dark" onClick={capture}>Dance, Dance</Button>
-                </Row></Col>
-            {/* <Col><Row><MusicComponent play={false} /></Row></Col> */}
-        </Container>
 
+    return (
+        <div>
+        <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={window.width}
+            videoConstraints={videoConstraints}
+            capture={capture}
+        />
+        <Button onClick={capture}>Dance, Dance</Button>
+        </div>
     );
 }
 
