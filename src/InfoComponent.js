a18de2160bd3bbbaf817752e796c5c3f246042e5
James <jameselson19@gmail.com>
2019-11-17 11:07:11 +0000

    Pretty

--- src/InfoComponent.js
+++ src/InfoComponent.js
@@ -14,7 +14,7 @@ export default class InfoComponent extends React.Component {
         super(props);
         this.state = {
             titleText: "How to Play",
-            bodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices iaculis massa, ut finibus velit euismod eu. Sed vehicula tortor auctor ultrices bibendum. Pellentesque a ligula aliquet, euismod augue congue, blandit leo. Donec sit amet nulla ac urna mollis ullamcorper. Cras nisl diam, tincidunt ut arcu vitae, posuere commodo urna. Morbi ac blandit tellus. Maecenas porttitor finibus mollis. Sed iaculis pellentesque augue, id dignissim risus semper quis. Vivamus et rutrum tellus. Integer ut ipsum lacus. Cras laoreet, arcu nec dictum scelerisque, dolor lorem semper nisl, a mollis tortor augue ac nulla. Nunc lacus lectus, tempus quis ex sit amet, condimentum dictum felis. Sed ultrices iaculis purus, at ornare leo iaculis eu. Nullam suscipit tincidunt purus vitae convallis. Nam condimentum mi sit amet enim semper, quis porttitor est placerat. Vestibulum pharetra lacinia elit, consequat suscipit dolor interdum ut.',
+            bodyText: 'To play the game, select a song and player 1 stand in front of the camera ready to dance! Next player 1 should press the "Dance PlayerA" button to record their cool moves :). Next player 2 should stand in front of the camera and press the "Dance PlayerB" button to attempt to copy player 1s dance moves.',
             info: true,
             p1Score : 0,
             p2Score : 0
